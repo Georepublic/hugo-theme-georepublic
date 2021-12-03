220a4e9946691eb7a7890eb47afcab937c4992d9
Daniel Kastl <daniel@georepublic.de>
2021-12-04 00:20:25 +0900

    Init chatwoot fix
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -74,26 +74,28 @@
   }
 
   (function(d,t) {
-    var TOKEN = document.getElementById("chatbotToken").value;
-    if (TOKEN != "unset") {
-      var BASE_URL = document.getElementById("chatbotUrl").value;
-      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-      g.src=BASE_URL+"/packs/js/sdk.js";
-      g.defer = true;
-      g.async = true;
-      s.parentNode.insertBefore(g,s);
-      g.onload=function(){
-        window.chatwootSettings = {
-          hideMessageBubble: false,
-          position: 'left',
-          locale: document.getElementById("chatbotLocale").value,
-          type: 'expanded_bubble',
-          showPopoutButton: false,
-        };
-        window.chatwootSDK.run({
-          websiteToken: TOKEN,
-          baseUrl: BASE_URL
-        })
+    if(document.getElementById("chatbotToken")){
+      var TOKEN = document.getElementById("chatbotToken").value;
+      if (TOKEN != "unset") {
+        var BASE_URL = document.getElementById("chatbotUrl").value;
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=BASE_URL+"/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+          window.chatwootSettings = {
+            hideMessageBubble: false,
+            position: 'left',
+            locale: document.getElementById("chatbotLocale").value,
+            type: 'expanded_bubble',
+            showPopoutButton: false,
+          };
+          window.chatwootSDK.run({
+            websiteToken: TOKEN,
+            baseUrl: BASE_URL
+          })
+        }
       }
     }
   })(document,"script");
